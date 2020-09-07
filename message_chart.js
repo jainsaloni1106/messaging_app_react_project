11008459195c9a5719abeafc4c67b02ea8267d78
jainsaloni1106 <48788350+jainsaloni1106@users.noreply.github.com>
2020-09-07 13:49:39 +0530

    Create message_chart.js

---
+++ message_chart.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import MessageView from './message-view';
+
+class MessageList extends Component {
+
+  state = {
+    messages:  [
+      {
+        from: 'John',
+        message: 'The event will start next week',
+        status: 'unread'
+      },
+      {
+        from: 'Martha',
+        message: 'I will be travelling soon',
+        status: 'read'
+      },
+      {
+        from: 'Jacob',
+        message: 'Talk later. Have a great day!',
+        status: 'read'
+      }
+    ]
+  }
+
+  render() {
+    const messages  = this.state.messages.map(function(message, index) {
+      return(
+        <MessageView key={index} message={message} />
+      )
+    })
+    return(
+      <div>
+        <h1>List of Messages</h1>
+        {messages}
+      </div>
+    )
+  }
+}
+
+export default MessageList
